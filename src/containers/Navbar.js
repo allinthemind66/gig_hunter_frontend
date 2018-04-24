41d2fa04a6fb078efec8f99bef0b5726b4f6fe78
allinthemind66 <r.nilon92@gmail.com>
2018-04-24 17:55:52 -0400

    added navbar and set up basic routing

---
+++ src/containers/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Navbar extends React.Component {
+  render(){
+    return(
+      <div>
+        <nav className="navbar navbar-light">
+          <span className="navbar-brand mb-0 h1"><h1><a href='/'>Gig Hunter</a></h1></span>
+          <span>
+            <button className='ui blue button' id="all-trips">Home</button>
+            <button className='ui blue button' id="add-trip">Search Gigs</button>
+            <button className='ui blue button' id="add-trip">Post Gig</button>
+            <button className='ui blue button' id="add-trip">Logout</button>
+          </span>
+        </nav>
+      </div>
+    )
+  }
+}
